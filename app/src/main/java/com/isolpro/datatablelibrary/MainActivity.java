de208233d47c7e6ce65247c57cc10e69b242ed40
Utsav Barnwal <u.barnwal36@gmail.com>
2021-02-07 21:00:06 +0530

    remeausing row_header view widths for body

--- app/src/main/java/com/isolpro/datatablelibrary/MainActivity.java
+++ app/src/main/java/com/isolpro/datatablelibrary/MainActivity.java
@@ -1,14 +1,61 @@
 package com.isolpro.datatablelibrary;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.HorizontalScrollView;
+import android.widget.RelativeLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+  private TableLayout rowHeader, colHeader, body;
+
+  private RelativeLayout layoutCorner, layoutColHeader, layoutRowHeader, layoutBody;
+
+  private HorizontalScrollView horizontalScrollBody, horizontalScrollRowHeader;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    layoutCorner = findViewById(R.id.layoutCorner);
+    layoutColHeader = findViewById(R.id.layoutColHeader);
+    layoutRowHeader = findViewById(R.id.layoutRowHeader);
+    layoutBody = findViewById(R.id.layoutBody);
+
+    rowHeader = layoutRowHeader.findViewById(R.id.table);
+    colHeader = layoutColHeader.findViewById(R.id.table);
+    body = layoutBody.findViewById(R.id.table);
+
+    body.post(this::syncBodyWidthWithRowHeader);
+  }
+
+  private void syncBodyWidthWithRowHeader() {
+    Log.e("body_width: ", String.valueOf(body.getWidth()));
+    Log.e("row_header_width: ", String.valueOf(rowHeader.getMeasuredWidth()));
+
+    TableRow firstBodyRow = (TableRow) body.getChildAt(0);
+    TableRow rowHeaderRow = (TableRow) rowHeader.getChildAt(0);
+
+    for (int i = 0; i < firstBodyRow.getChildCount(); i++) {
+      View bodyCell = firstBodyRow.getChildAt(i);
+      View rowHeaderCell = rowHeaderRow.getChildAt(i);
+
+      if (bodyCell.getMeasuredWidth() > rowHeaderCell.getMeasuredWidth())
+        rowHeaderCell.setMinimumWidth(bodyCell.getMeasuredWidth());
+      else
+        bodyCell.setMinimumWidth(rowHeaderCell.getMeasuredWidth());
+    }
+
+    rowHeader.post(() -> {
+      Log.e("body_width: ", String.valueOf(body.getWidth()));
+      Log.e("row_header_width: ", String.valueOf(rowHeader.getMeasuredWidth()));
+    });
+
   }
 }
